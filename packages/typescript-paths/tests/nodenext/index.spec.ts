da0215153c43d60ccefbef2c5b4145c471a23937
lightyen <lightyen0123@gmail.com>
2022-07-25 13:13:32 +0800

    test 'moduleSuffixes'

--- packages/typescript-paths/tests/nodenext/index.spec.ts
+++ packages/typescript-paths/tests/nodenext/index.spec.ts
@@ -6,6 +6,8 @@ test("resolving", async () => {
 	expect(handler).toBeTruthy()
 	if (!handler) return
 	const resolve = (request: string) => handler(request, path.resolve(__dirname, "main.tsx"))
+	expect(resolve("~/App")).toEqual(path.resolve(__dirname, "src/App.tsx"))
 	expect(resolve("~/App.js")).toEqual(path.resolve(__dirname, "src/App.tsx"))
 	expect(resolve("~/App.jsx")).toEqual(path.resolve(__dirname, "src/App.tsx"))
+	expect(resolve("~/Svg.svg")).toEqual(path.resolve(__dirname, "src/Svg.svg.ts"))
 })
--- packages/typescript-paths/tests/nodenext/main.tsx
+++ packages/typescript-paths/tests/nodenext/main.tsx
@@ -1,2 +1,4 @@
-import App from "~/App.jsx"
+import App from "~/App"
+import { S } from "~/Svg.svg"
 App
+S
