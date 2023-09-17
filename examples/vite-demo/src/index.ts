2a353f2bf3428b9d49a036fcb96b8844b28f0b21
lightyen <lightyen0123@gmail.com>
2023-09-17 16:04:15 +0800

    update examples/demo

--- examples/vite-demo/src/index.ts
+++ examples/vite-demo/src/index.ts
@@ -1,2 +1,4 @@
 import { value } from "he"
 console.log(value)
+import { value as v } from "/hello"
+console.log(v)
--- examples/vite-demo/vite.config.ts
+++ examples/vite-demo/vite.config.ts
@@ -5,5 +5,5 @@ export default defineConfig({
 	build: {
 		outDir: "build",
 	},
-	plugins: [tsConfigPaths({ tsConfigPath: "src/tsconfig.json" })],
+	plugins: [tsConfigPaths()],
 })
